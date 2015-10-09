fce5fcc94a7c68852f235f1b39b068a315fa7c24
Bindiya Rana <bindiya02rana@gmail.com>
2015-10-08 20:31:13 -0400

    Update Hellofromvb.aspx.vb

--- Hellofromvb.aspx.vb
+++ Hellofromvb.aspx.vb
@@ -1,5 +1,5 @@
 
 Partial Class Hellofromvb
     Inherits System.Web.UI.Page
-    Protected var1 As String = "Hello from VB"
+    Protected var1 As String = "Hello from VB" 'this is how you create variable into vb'
 End Class
