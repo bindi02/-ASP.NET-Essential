e83b97189f215e202be14da38b827ad08b05e0e7
Bindiya Rana <bindiya02rana@gmail.com>
2015-10-08 20:41:03 -0400

    this c# thats how u call variable

---
+++ Hellofromcsharp.aspx.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Hellofromcsharp : System.Web.UI.Page
+{ 
+    // u declare variable here above that page load  always end with semi stuff at the end 
+    protected string var1 = "Hello frm c#";
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+}
