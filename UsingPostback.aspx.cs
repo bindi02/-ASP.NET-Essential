ec6f2857d60c1dc454de24f77aab80c19b044cf4
Bindiya Rana <bindiya02rana@gmail.com>
2015-10-10 14:47:29 -0400

    that code for panel which get that value frm input and add on label

---
+++ UsingPostback.aspx.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class UsingPostback : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+       
+    }
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+      fnameLabel.Text = fnameInput.Text;
+      lnameLabel.Text = lnameInput.Text;
+      dobLabel.Text = dobInput.Text;  
+    }
+}
